006aa764548c9a83c69870b43b5e09d557188ac0
zhiken <zhiken03@hotmail.com>
2024-09-02 16:20:03 +0800

    init

---
+++ SEM.Prototype/SEM.Prototype/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SEM.Prototype.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
