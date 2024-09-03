8b6da2b9754f3151dcb1d22981cebd8d59eb7d77
zhiken <zhiken03@hotmail.com>
2024-09-03 21:11:33 +0800

    setup signalR, add signalR client-side lib

---
+++ SEM.Prototype/SEM.Prototype/Controllers/ChatbotController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SEM.Prototype.Controllers
+{
+    [Route("[Controller]")]
+    public class ChatbotController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SEM.Prototype/SEM.Prototype/Hubs/ChatbotHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SEM.Prototype.Hubs
+{
+    public class ChatbotHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
