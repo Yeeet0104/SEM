7a2e74c5f75984384fec2cf55cd8be286033c0bb
Dexter <dextergoh03@gmail.com>
2024-09-19 14:37:45 +0800

    ok

---
+++ SEM.Prototype/SEM.Prototype/Models/EventModel .cs
@@ -0,0 +1,11 @@
+﻿namespace SEM.Prototype.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+        public string Description { get; set; }
+        public bool AllDay { get; set; }  // Add this field
+    }
+}
