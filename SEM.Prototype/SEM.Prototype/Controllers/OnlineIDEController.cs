e863bb3c7ac03de7ceedc524280f317212274375
veo <goodmanga118@gmail.com>
2024-09-21 18:51:44 +0800

    Created Online IDE for Python Language. TO DO: Update so that can accept input.

---
+++ SEM.Prototype/SEM.Prototype/Controllers/OnlineIDEController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEM.Prototype.Services.OnlineIDE;
+using System;
+using System.Threading.Tasks;
+
+namespace SEM.Prototype.Controllers
+{
+    public class OnlineIDEController : Controller
+    {
+        private readonly CodeExecutionService _codeExecutionService;
+
+        public OnlineIDEController(CodeExecutionService codeExecutionService)
+        {
+            _codeExecutionService = codeExecutionService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RunCode([FromBody] CodeExecutionRequest request)
+        {
+            try
+            {
+                var output = await _codeExecutionService.ExecuteCodeAsync(request.Code, request.Language);
+                return Json(new { success = true, output = output });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, output = $"An error occurred: {ex.Message}" });
+            }
+        }
+    }
+
+    public class CodeExecutionRequest
+    {
+        public string Code { get; set; }
+        public string Language { get; set; }
+    }
+}
